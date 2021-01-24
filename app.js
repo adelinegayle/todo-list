e6978892bbc2b720e168c9764a9ba191ccd52c77
Adeline Gayle Noman <nomanal@ph.ibm.com>
2021-01-24 04:09:07 -0500

    Fix casing error

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ app.get('/about', function(req, res) {
   res.render('about');
 });
 
-let port = process.env.Port;
+let port = process.env.PORT;
 
 if (port == null || port == '') {
   port = 3000;
